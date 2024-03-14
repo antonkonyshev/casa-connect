9bd56765d056f37c56f2b02b1f4b9cf2bd8afe2e
Anton Konyshev <anton.konyshev@gmail.com>
2024-03-14 09:13:26 +0300

    Measurement history chart refactored. Temperature and pollution charts have been added.

--- app/src/main/java/name/antonkonyshev/home/presentation/meteo/MeteoComposable.kt
+++ app/src/main/java/name/antonkonyshev/home/presentation/meteo/MeteoComposable.kt
@@ -1,6 +1,5 @@
 package name.antonkonyshev.home.presentation.meteo
 
-import android.content.Context
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -19,7 +18,6 @@ import androidx.compose.material.icons.filled.Sensors
 import androidx.compose.material.icons.filled.SensorsOff
 import androidx.compose.material.icons.outlined.Landscape
 import androidx.compose.material.icons.outlined.Masks
-import androidx.compose.material.icons.outlined.SsidChart
 import androidx.compose.material.icons.outlined.Thermostat
 import androidx.compose.material.icons.outlined.TireRepair
 import androidx.compose.material.pullrefresh.PullRefreshIndicator
@@ -37,22 +35,11 @@ import androidx.compose.runtime.State
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.viewinterop.AndroidView
-import com.github.mikephil.charting.charts.LineChart
-import com.github.mikephil.charting.components.XAxis
-import com.github.mikephil.charting.data.Entry
-import com.github.mikephil.charting.data.LineData
-import com.github.mikephil.charting.data.LineDataSet
-import com.github.mikephil.charting.formatter.IFillFormatter
-import com.github.mikephil.charting.formatter.IndexAxisValueFormatter
-import com.github.mikephil.charting.interfaces.dataprovider.LineDataProvider
-import com.github.mikephil.charting.interfaces.datasets.ILineDataSet
 import name.antonkonyshev.home.R
 import name.antonkonyshev.home.domain.entity.Device
 import name.antonkonyshev.home.domain.entity.Measurement
@@ -150,68 +137,78 @@ fun MeteoScreen(
                                 Column {
                                     Row {
                                         SensorValueListItem(
-                                            label = stringResource(R.string.temperature),
+                                            label = stringResource(R.string.air_temperature),
                                             icon = Icons.Outlined.Thermostat,
                                             sensorValue = measurements[device.id]!!.value.temperature,
                                             units = stringResource(R.string.c),
+                                            modifier = Modifier.padding(20.dp, 20.dp, 20.dp, 0.dp)
                                         )
                                     }
 
-                                    Row {
-                                        SensorValueListItem(
-                                            label = stringResource(R.string.pressure),
-                                            icon = Icons.Outlined.TireRepair,
-                                            sensorValue = measurements[device.id]!!.value.pressure,
-                                            units = stringResource(R.string.mmhg),
-                                        )
-                                    }
-
-                                    Row {
-                                        SensorValueListItem(
-                                            label = stringResource(R.string.pollution),
-                                            icon = Icons.Outlined.Masks,
-                                            sensorValue = measurements[device.id]!!.value.pollution,
-                                            units = stringResource(R.string.mg_m3),
-                                        )
-                                    }
-
-                                    Row {
-                                        SensorValueListItem(
-                                            label = stringResource(R.string.altitude),
-                                            icon = Icons.Outlined.Landscape,
-                                            sensorValue = measurements[device.id]!!.value.altitude,
-                                            units = stringResource(R.string.m),
-                                        )
-                                    }
-
-                                    if (histories[device.id] is State<List<Measurement>> &&
+                                    if (
+                                        histories[device.id] is State<List<Measurement>> &&
                                         measurements[device.id] is State<Measurement> &&
                                         (histories[device.id]?.value?.size ?: 0) > 2
                                     ) {
-                                        Row {
-                                            Column {
-                                                Row(
-                                                    Modifier.padding(20.dp, 20.dp, 20.dp, 0.dp),
-                                                    verticalAlignment = Alignment.CenterVertically
-                                                ) {
-                                                    Icon(
-                                                        imageVector = Icons.Outlined.SsidChart,
-                                                        contentDescription = stringResource(
-                                                            R.string.temperature_over_time
-                                                        ),
-                                                        modifier = Modifier.padding(end = 20.dp)
-                                                    )
-                                                    Text(
-                                                        text = stringResource(R.string.temperature_over_time),
-                                                        style = MaterialTheme.typography.labelMedium,
-                                                    )
-                                                }
-
-                                                MeasurementHistoryChart(
-                                                    device, histories, measurements
-                                                )
-                                            }
+                                        Row(Modifier.height(120.dp)) {
+                                            MeasurementHistoryChart(
+                                                histories[device.id]!!.value.map {
+                                                    it.temperature ?: 0f
+                                                }.toList(),
+                                                measurements[device.id]!!.value.temperature ?: 0f,
+                                                label = stringResource(R.string.temperature),
+                                                units = stringResource(R.string.c)
+                                            )
                                         }
+                                    }
+
+                                    Row {
+                                        SensorValueListItem(
+                                            label = stringResource(R.string.air_pollution),
+                                            icon = Icons.Outlined.Masks,
+                                            sensorValue = measurements[device.id]!!.value.pollution,
+                                            units = stringResource(R.string.mg_m3),
+                                            modifier = Modifier.padding(20.dp, 40.dp, 20.dp, 0.dp)
+                                        )
+                                    }
+
+                                    if (
+                                        histories[device.id] is State<List<Measurement>> &&
+                                        measurements[device.id] is State<Measurement> &&
+                                        (histories[device.id]?.value?.size ?: 0) > 2
+                                    ) {
+                                        Row(Modifier.height(120.dp)) {
+                                            MeasurementHistoryChart(
+                                                histories[device.id]!!.value
+                                                    .map { it.pollution ?: 0f }.toList(),
+                                                measurements[device.id]!!
+                                                    .value.pollution ?: 0f,
+                                                label = stringResource(R.string.pollution),
+                                                units = stringResource(R.string.mg_m3),
+                                                color = Color.Blue,
+                                                granularity = 1f
+                                            )
+                                        }
+                                    }
+
+                                    Row {
+                                        SensorValueListItem(
+                                            label = stringResource(R.string.atmosphere_pressure),
+                                            icon = Icons.Outlined.TireRepair,
+                                            sensorValue = measurements[device.id]!!.value.pressure,
+                                            units = stringResource(R.string.mmhg),
+                                            modifier = Modifier.padding(20.dp, 40.dp, 20.dp, 20.dp)
+                                        )
+                                    }
+
+                                    Row {
+                                        SensorValueListItem(
+                                            label = stringResource(R.string.altitude_above_sea_level),
+                                            icon = Icons.Outlined.Landscape,
+                                            sensorValue = measurements[device.id]!!.value.altitude,
+                                            units = stringResource(R.string.m),
+                                            modifier = Modifier.padding(20.dp)
+                                        )
                                     }
                                 }
                             }
@@ -230,109 +227,6 @@ fun MeteoScreen(
 }
 
 @Composable
-private fun MeasurementHistoryChart(
-    device: Device,
-    histories: Map<String, State<List<Measurement>>>,
-    measurements: Map<String, State<Measurement>>
-) {
-    AndroidView(
-        modifier = Modifier
-            .height(150.dp)
-            .fillMaxSize(),
-        factory = { ctx ->
-            prepareChartView(ctx).apply {
-                setDrawMarkers(false)
-                data = LineData(
-                    prepareChartDataSet(
-                        prepareEntries(
-                            histories[device.id]!!.value, measurements[device.id]!!.value
-                        ), axisRight.axisMinimum
-                    )
-                ).apply {
-                    setDrawValues(false)
-                }
-                invalidate()
-            }
-        }
-    ) { chartView ->
-        chartView.apply {
-            val entries = prepareEntries(
-                histories[device.id]!!.value, measurements[device.id]!!.value
-            )
-            if (data != null && data.dataSetCount > 0) {
-                (data.getDataSetByIndex(0) as LineDataSet).values = entries
-            } else {
-                data = LineData(prepareChartDataSet(entries, axisRight.axisMinimum)).apply {
-                    setDrawValues(false)
-                }
-            }
-            data.notifyDataChanged()
-            notifyDataSetChanged()
-            invalidate()
-        }
-    }
-}
-
-private fun prepareChartView(ctx: Context): LineChart {
-    return LineChart(ctx).apply {
-        description.isEnabled = false
-        setTouchEnabled(false)
-        isDragEnabled = false
-        setScaleEnabled(false)
-        legend.isEnabled = false
-        xAxis.position = XAxis.XAxisPosition.BOTH_SIDED
-        xAxis.setDrawGridLines(false)
-        xAxis.setDrawLabels(false)
-        axisLeft.isEnabled = true
-        axisLeft.setDrawLabels(false)
-        axisRight.valueFormatter = object : IndexAxisValueFormatter() {
-            override fun getFormattedValue(
-                value: Float
-            ): String {
-                return "${String.format("%.2f", value)} °C"
-            }
-        }
-    }
-}
-
-private fun prepareEntries(
-    history: List<Measurement>,
-    measurement: Measurement
-): MutableList<Entry> {
-    return history.mapIndexed { idx, record ->
-        Entry(idx.toFloat(), record.temperature ?: 0f)
-    }.toMutableList().apply {
-        add(Entry(size.toFloat(), measurement.temperature ?: 0f))
-    }
-}
-
-// TODO: try to add pollution and pressure charts, check if it'll look fine or not
-private fun prepareChartDataSet(entries: MutableList<Entry>, yAxisMinimum: Float): LineDataSet {
-    return LineDataSet(entries, "Temperature").apply {
-        mode = LineDataSet.Mode.CUBIC_BEZIER
-        cubicIntensity = 0.2f
-        setDrawFilled(true)
-        setDrawCircles(false)
-        lineWidth = 3f
-        circleRadius = 4f
-        setCircleColor(Color.White.toArgb())
-        highLightColor = Color.Red.toArgb()
-        setColor(Color.Red.copy(alpha = 0.7f).toArgb())
-        fillColor = Color.Red.toArgb()
-        fillAlpha = 75
-        setDrawHorizontalHighlightIndicator(false)
-
-        setFillFormatter(object : IFillFormatter {
-            override fun getFillLinePosition(
-                dataSet: ILineDataSet?, dataProvider: LineDataProvider?
-            ): Float {
-                return yAxisMinimum
-            }
-        })
-    }
-}
-
-@Composable
 fun SensorValueListItem(
     label: String,
     icon: ImageVector,
@@ -341,9 +235,7 @@ fun SensorValueListItem(
     modifier: Modifier = Modifier
 ) {
     Row(
-        modifier = modifier
-            .padding(20.dp)
-            .fillMaxWidth(),
+        modifier = modifier.fillMaxWidth(),
         verticalAlignment = Alignment.CenterVertically,
     ) {
         Icon(
